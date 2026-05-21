c223a83e54139108458b50c9ca528b96f48fcc86
manjeetops <lyajeans37@gmail.com>
2026-05-21 15:47:42 +0530

    feat: initialize node.js project with typescript and eslint config

---
+++ app/eslint.config.js
@@ -0,0 +1,21 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
